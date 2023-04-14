b0819d04fce16cac728e93274367aa9e8cf1d97f
HansumaeCecilia <48033@edu.raseko.fi>
2023-04-14 11:23:23 +0300

    Ruled added for emabling calculation

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # LIBRARIES AND MODULES
 import sys
 from PyQt5 import QtCore # Core functionality of Qt
-from PyQt5 import QtWidgets # UI elements' functionality
+from PyQt5 import QtWidgets as QW # UI elements' functionality
 from PyQt5.uic import loadUi
 import kuntoilija
 import fitness
@@ -13,7 +13,7 @@ import timetools
 # TODO: Import some library able to plot trends and make it as widget in the UI
 
 # Class for the main window
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QW.QMainWindow):
     
     """MainWindow for the fitness app"""
 
@@ -25,11 +25,17 @@ class MainWindow(QtWidgets.QMainWindow):
         self.main = loadUi('main.ui', self)
 
         # Define UI Controls, ie. buttons and input fields
-        self.nameLE = self.nameLineEdit
+        self.nameLE = self.findChild(QW.QLineEdit, 'nameLineEdit')        
+        self.nameLE.textEdited.connect(self.activateCalculatePB)
+
         self.birthDE = self.birthDateEdit
+        self.birthDE.dateChanged.connect(self.activateCalculatePB)
         self.genderCB = self.genderComboBox
+        self.genderCB.currentTextChanged.connect(self.activateCalculatePB)
+
         self.measuringDE = self.measuringDateEdit
-        self.measuringDE.setDate(QtCore.QDate.currentDate())        
+        self.measuringDE.setDate(QtCore.QDate.currentDate())
+
         self.heightDSB = self.heightDoubleSpinBox
         self.weightDSB = self.weightDoubleSpinBox
         self.neckSB = self.neckSpinBox
@@ -37,17 +43,35 @@ class MainWindow(QtWidgets.QMainWindow):
         self.pelvisSB = self.pelvisSpinBox
 
         # TODO: Disable Calculate button until values have been edited
-        self.calculatePB = self.calculatePushButton
-        self.calculatePB.clicked.connect(self.calculateAll)        
-        self.savePB = self.savePushButton
+        self.calculatePB = self.findChild(QW.QPushButton, 'calculatePushButton')
+        self.calculatePB.clicked.connect(self.calculateAll)
+        self.calculatePB.setEnabled(False)           
+
+        # TODO: Disable Save button until values have been calculated     
+        #self.savePB = self.savePushButton
+        self.savePB = self.findChild(QW.QPushButton, 'savePushButton')
         self.savePB.clicked.connect(self.saveData)
+        self.savePB.setEnabled(False)
 
     # Define slots, ie. methods
+    def activateCalculatePB(self):
+        self.calculatePB.setEnabled(True)
+        if self.nameLE.text() == '':
+            self.calculatePB.setEnabled(False)
+        
+        if self.birthDE.date() == QtCore.QDate(1900, 1, 1):
+            self.calculatePB.setEnabled(False)
+    
+        if self.genderCB.currentText() == '':
+            self.calculatePB.setEnabled(False)
+
     # Calculates BMI, Finnish and US fat percetanges and updates corresponding labels
     def calculateAll(self):
         name = self.nameLE.text()
         height = self.heightDSB.value() # Spinbox value as integer
         weight = self.weightDSB.value()
+        self.calculatePB.setEnabled(False)
+        self.savePB.setEnabled(True)
 
         # Convert birthday to ISO string using QtCore's methods
         birthday = self.birthDE.date().toString(format=QtCore.Qt.ISODate)        
@@ -55,10 +79,10 @@ class MainWindow(QtWidgets.QMainWindow):
         # Set gender value according to ComboBox value
         gendertext = self.genderCB.currentText()
         if gendertext == 'Mies':
-            gender = 1
+            gender = 1      
         else:
-            gender = 0
-        
+            gender = 0       
+                            
         # Convert measuring data to ISO string
         measuringDate = self.measuringDE.date().toString(format=QtCore.Qt.ISODate)
         
@@ -79,7 +103,7 @@ class MainWindow(QtWidgets.QMainWindow):
 if __name__ == "__main__":
 
     # Create the application
-    app = QtWidgets.QApplication(sys.argv)
+    app = QW.QApplication(sys.argv)
 
   # Create the Main Window object from MainWindow class and show it on the screen
     appWindow = MainWindow()
