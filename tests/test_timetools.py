078c4165e2dd28de1b85da1edc97304eed32433f
HansumaeCecilia <48033@edu.raseko.fi>
2023-04-28 13:19:47 +0300

    Added test for dateTimeDiff

--- tests/test_timetools.py
+++ tests/test_timetools.py
@@ -12,9 +12,11 @@ def test_datediff():
     assert timetools.datediff('2023-04-10', '2023-04-28') == 18
 
 # Test if timediff function calclulates correct and absolute values
-
 def test_timediff():
-    assert timetools.timediff(('10:10:05', '11:30:15'), 4) == 1.3361
-    assert timetools.timediff(('11:30:15', '10:10:05'), 4) == 1.3361
+    assert round(timetools.timediff('11:30:15', '10:10:05'), 4) == 1.3361
+    assert round(timetools.timediff('10:10:05', '11:30:15'), 4) == 1.3361
 
-    # (('10:10:05', '11:30:15'), 4) == 1.3361
+# Test if dateTimeDiff works correctly
+def test_dateTimeDiff():
+    assert timetools.dateTimeDiff('2023-04-28 12:30:00', '2023-04-27 10:00:00') == 26.5
+    assert timetools.dateTimeDiff('2023-04-27 10:00:00', '2023-04-28 12:30:00') == 26.5
