3eaa78e15667fa3cd8a9e03bbcfa9c09fc518936
HansumaeCecilia <48033@edu.raseko.fi>
2023-05-05 15:07:37 +0300

    Imported insctructions, restore defaults code, msg boxes

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from PyQt5.uic import loadUi
 import kuntoilija
 import timetools
 import athlete_file # Home made module for processing data files
+import instructions
 
 # TODO: Import some library able to plot trends and make it as widget in the UI
 
@@ -50,6 +51,10 @@ class MainWindow(QW.QMainWindow):
         self.hipSB.setEnabled(False)
         self.hipSB.valueChanged.connect(self.activateCalculatePB)
 
+        self.bmiLE = self.bmiLabel
+        self.fatFiLE = self.fatFiLabel
+        self.fatUsLE = self.fatUsLabel
+
         # Create a status bar for showing informational messages
         self.statusBar = QW.QStatusBar()
         self.setStatusBar(self.statusBar)
@@ -60,7 +65,11 @@ class MainWindow(QW.QMainWindow):
         self.calculatePB.clicked.connect(self.calculateAll)
         self.calculatePB.setEnabled(False)           
 
-        # self.savePB = self.savePushButton
+        # Temporary push button for inserting test values into controls
+        self.testPB = self.testUiPushButton
+        self.testPB.clicked.connect(self.insertTestValues)
+        
+        # A push button for saving user data
         self.savePB = self.findChild(QW.QPushButton, 'savePushButton')
         self.savePB.clicked.connect(self.saveData)
         self.savePB.setEnabled(False)
@@ -72,8 +81,57 @@ class MainWindow(QW.QMainWindow):
             data = jsonFile.readData('athleteData.json')
             self.dataList = data[3]
         except Exception as e:
-            data = (1, 'Error', str(e), self.dataList)              
-        
+            data = (1, 'Error', str(e), self.dataList)     
+
+        # MENU ACTIONS
+        self.actionRestoreDefaults.triggered.connect(self.restoreDefaults)  
+        self.actionInstructions.triggered.connect(self.openHelpDialog)
+    
+    # Create an alerting method
+    def alert(self, windowTitle, message, detailedMessage):
+        msgBox = QW.QMessageBox()
+        msgBox.setIcon(QW.QMessageBox.Critical)
+        msgBox.setWindowTitle(windowTitle)
+        msgBox.setText(message)
+        msgBox.setDetailedText(detailedMessage)
+        msgBox.exec()
+
+    # Create a warning method
+    def warn(self, windowTitle, message, detailedMessage):
+        msgBox = QW.QMessageBox()
+        msgBox.setIcon(QW.QMessageBox.Warning)
+        msgBox.setWindowTitle(windowTitle)
+        msgBox.setText(message)
+        msgBox.setDetailedText(detailedMessage)
+        msgBox.exec()
+
+    # Create an information method  
+    def inform(self, windowTitle, message, detailedMessage):
+        msgBox = QW.QMessageBox()
+        msgBox.setIcon(QW.QMessageBox.Information)
+        msgBox.setWindowTitle(windowTitle)
+        msgBox.setText(message)
+        msgBox.setDetailedText(detailedMessage)
+        msgBox.exec()
+
+    def showMessageBox(self, windowTitle, message, detailedMessage, icon='Information'):
+        """Shows icon depending on error message
+
+        Args:
+            windowTitle (str): Header for the message window 
+            message (str): Message to be shown
+            detailedMessage (str): A message that can be viewed by pressing the 'details' button
+            icon (str, optional): Allowed values: NoIcon, Information, Question, Warning and Critical
+        """
+        iconTypes = {'Information': QW.QMessageBox.Information, 'NoIcon': QW.QMessageBox.NoIcon, 
+         'Question': QW.QMessageBox.Question, 'Warning': QW.QMessageBox.Warning, 'Critical': QW.QMessageBox.Critical}
+        msgBox = QW.QMessageBox()
+        msgBox.setIcon(iconTypes[icon])
+        msgBox.setWindowTitle(windowTitle)
+        msgBox.setText(message)
+        msgBox.setDetailedText(detailedMessage)
+        msgBox.exec()
+
     # Define slots, ie. methods
     def activateCalculatePB(self):
         self.calculatePB.setEnabled(True)
@@ -112,6 +170,17 @@ class MainWindow(QW.QMainWindow):
             self.minHeightLE.setVisible(True)    
     '''    
         
+    def insertTestValues(self):
+        # Set test values to all controls
+        self.nameLE.setText('Teppo Testi')
+        testBirthDay = QtCore.QDate(1999, 12, 31)
+        self.birthDE.setDate(testBirthDay)
+        self.genderCB.setCurrentText('Mies')
+        self.heightDSB.setValue(171)
+        self.weightDSB.setValue(75)
+        self.neckSB.setValue(27)
+        self.waistSB.setValue(90)
+
     # Calculates BMI, Finnish and US fat percetanges and updates corresponding labels
     def calculateAll(self):
         name = self.nameLE.text()
@@ -134,16 +203,20 @@ class MainWindow(QW.QMainWindow):
         measuringDate = self.measuringDE.date().toString(format=QtCore.Qt.ISODate)
         
         # Calculate time difference with our homemade tools
-        age = timetools.datediff2(birthday, measuringDate, 'year')
-
+        age = timetools.datediff2(birthday, measuringDate, 'year')        
         neck = self.neckSB.value()
+        if neck < 21:
+            # self.alert('Tarkista kaulan ympärysmitta', 'Kaulan ympärysmitta liian pieni',
+            #             'Kaulan koko voi olla välillä 21 - 60 cm')
+            self.showMessageBox('Tarkista kaulan ympärysmitta', 'Kaulan ympärysmitta virheellinen',
+                                'Sallitut arvot 21 - 60 cm', 'Warning')
         waist = self.waistSB.value()
         hip = self.hipSB.value()
 
         athlete = kuntoilija.Kuntoilija(name, height, weight, age, gender, neck, waist, hip, measuringDate)
         
         bmi = athlete.bmi
-        self.showBmiLabel.setText(str(bmi))      
+        self.bmiLabel.setText(str(bmi))      
         
         fiFatPercentage = athlete.fi_rasva
         usaFatPercentage = athlete.usa_rasva
@@ -167,7 +240,20 @@ class MainWindow(QW.QMainWindow):
         # Save list to a json file
         jsonfile2 = athlete_file.ProcessJsonFile()
         status = jsonfile2.saveData('athleteData.json', self.dataList)
-        self.StatusBar.showMessage(status[1], 4000)
+
+        # Show message about status of saving on statbar
+        self.statusBar.showMessage(status[1], 4000)
+
+        # Call error message box if error code is not 0
+        if status[0] != 0:
+            self.alert(status[1], status[2])
+        else:
+            self.restoreDefaults()
+
+        # TODO: Reset fat percentage values in UI after saving   
+
+    def restoreDefaults(self):
+        # Set all inputs to their default values after saving data
         zeroDate = QtCore.QDate(1900, 1, 1)
         self.birthDE.setDate(zeroDate)
         zeroDate2 = (QtCore.QDate.currentDate())
@@ -179,9 +265,14 @@ class MainWindow(QW.QMainWindow):
         self.neckSB.setValue(10)
         self.waistSB.setValue(30)
         self.hipSB.setValue(50)
-        self.savePB.setEnabled(False)
-
-        # TODO: Reset fat percentage values in UI after saving   
+        self.savePB.setEnabled(False)        
+        self.bmiLE.setText('0')
+        self.fatFiLE.setText('0')
+        self.fatUsLE.setText('0')
+    
+    def openHelpDialog(self):
+        openHelp = instructions.OpenHelp()
+        openHelp.exec()
 
 if __name__ == "__main__":
 
